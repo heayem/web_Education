c680b84b6135662f2ae161a7752f14abbac7b3d9
heayem <heayem7@gmail.com>
2023-07-07 23:48:41 +0700

    category setup

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8080
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-// app.use(cors({ origin: "*" }))
+app.use(cors({ origin: "*" }))
 
 
 require("./src/route/user.route")(app)
